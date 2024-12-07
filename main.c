4f7053f2770ccd7e0ad5c992a3b8db416ca4e461
yujian1904 <yujian1904@outlook.com>
2024-12-07 17:01:09 +0800

    20241207

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#define STR_SIZE 20
-struct Book
-{
-    
-};
-
+#define STR_SIZE 10
+char *mygets(FILE *stream);
 int main(int, char **)
 {
-    
+    char *str = mygets(stdin);
+    puts(str);
+
+    free(str);
     return 0;
+}
+
+char *mygets(FILE *stream)
+{
+    long long cur = 0;
+    int size = 5;
+    char *str = (char *)malloc(sizeof(char) * size);
+    char ch;
+    while ((ch = getc(stream)) != '\n' && ch != '\0')
+    {
+        if (cur < size)
+        {
+            str[cur] = ch;
+        }
+        else
+        {
+            size += 5;
+            str = (char *)realloc(str, size);
+            str[cur] = ch;
+        }
+        cur++;
+    }
+    if (ch == '\n')
+    {
+        str[cur] = '\0';
+    }
+    return str;
 }
